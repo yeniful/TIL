0f67c94e49b9d1d79989718083b87b0b863f5667
yeni <yeniful@icloud.com>
2022-06-06 20:52:38 +0900

    SwiftUI Tutorial

--- SwiftUITutorial/Landmarks/Landmarks/LandmarksApp.swift
+++ SwiftUITutorial/Landmarks/Landmarks/LandmarksApp.swift
@@ -11,10 +11,18 @@ import SwiftUI
 //The @main attribute identifies the app’s entry point.
 @main
 struct LandmarksApp: App {
+    
+//    Update the LandmarksApp to create a model instance and supply it to ContentView using the environmentObject(_:) modifier.
+    
+//    Use the @StateObject attribute to initialize a model object for a given property ⭐️only once⭐️ during the life time of the app. This is true when you use the attribute in an app instance, as shown here, as well as when you use it in a view.
+    
+    @StateObject var modelData = ModelData()
+    
 //    The structure’s body property returns one or more scenes
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
--- SwiftUITutorial/Landmarks/Landmarks/Model/ModelData.swift
+++ SwiftUITutorial/Landmarks/Landmarks/Model/ModelData.swift
@@ -6,8 +6,20 @@
 //
 
 import Foundation
+import Combine
 
-var landmarks: [Landmark] = load("landmarkData.json")
+// An observable object is a custom object for your data that can be bound to a view from storage in SwiftUI’s environment. SwiftUI watches for any changes to observable objects that could affect a view, and displays the correct version of the view after a change.
+
+// Declare a new model type that conforms to the ObservableObject protocol from the Combine framework.
+// SwiftUI subscribes to your observable object, and updates any views that need refreshing when the data changes.
+
+final class ModelData: ObservableObject {
+    
+//    An observable object needs to publish any changes to its data, so that its subscribers can pick up the change.
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
+
+
 
 func load<T: Decodable>(_ filename: String) -> T{
     let data: Data
